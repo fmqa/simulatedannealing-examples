cff7b2156fa2171f6e9fd58cc03aa1528f1a2065
Fadi Moukayed <moukayed@gmit-gmbh.de>
2018-06-30 20:13:07 +0200

    Adapt Main.kt header.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,7 @@
 /*
- * Simulated annealing on image demo (Java)
+ * Graphical simulated annealing demo.
  *
- * Copyright (c) 2017 Project Nayuki
- * All rights reserved. Contact Nayuki for licensing.
- * https://www.nayuki.io/page/simulated-annealing-demo
+ * Re-implementation of the original demo by Project Nayuki (nayuki.io).
  */
 
 import org.kochab.simulatedannealing.ExponentialDecayScheduler
@@ -14,30 +12,30 @@ import java.io.IOException
 import java.security.SecureRandom
 import javax.imageio.ImageIO
 
-object SimulatedAnnealingOnImage {
+object SAImage {
     @Throws(IOException::class)
     @JvmStatic
     fun main(args: Array<String>) {
         // Configurable parameters
-        val WIDTH = 256
-        val HEIGHT = 256
-        val NUM_ITERATIONS = 1000000000000
-        val START_TEMPERATURE = 100.0
+        val width = 256
+        val height = 256
+        val iterations = 1000000000000
+        val temp = 100.0
         val rng = SecureRandom()
 
         // Run the heavy computation
-        val problem = PixelProblem(WIDTH, HEIGHT, rng) {
+        val problem = PixelProblem(width, height, rng) {
             n, e, retry -> if (n % 1000 == 0) println("#$n\tE=$e\tRETRY=$retry")
         }
 
-        val solver = Solver(problem, ExponentialDecayScheduler(START_TEMPERATURE, NUM_ITERATIONS), rng)
+        val solver = Solver(problem, ExponentialDecayScheduler(temp, iterations), rng)
         val result = solver.solve()
 
         // Write output image
-        val image = BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB)
-        image.setRGB(0, 0, WIDTH, HEIGHT, problem.data, 0, WIDTH)
+        val image = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
+        image.setRGB(0, 0, width, height, problem.data, 0, width)
         val outputFilename = String.format("simulated-annealing-time%d-iters%d-starttemp%.1f.bmp",
-                System.currentTimeMillis(), NUM_ITERATIONS, START_TEMPERATURE)
+                System.currentTimeMillis(), iterations, temp)
         ImageIO.write(image, "bmp", File(outputFilename))
     }
 

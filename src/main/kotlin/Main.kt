97b87e0f489929dd015ee0515df7ecd4919dba81
Fadi Moukayed <moukayed@gmit-gmbh.de>
2018-07-03 18:30:20 +0200

    Adapt to API changes.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,6 +6,7 @@
 
 import org.kochab.simulatedannealing.ExponentialDecayScheduler
 import org.kochab.simulatedannealing.Solver
+import org.kochab.simulatedannealing.MinimumListener
 import java.awt.image.BufferedImage
 import java.io.File
 import java.io.IOException
@@ -16,7 +17,7 @@ object SAImage {
     @Throws(IOException::class)
     @JvmStatic
     fun main(args: Array<String>) {
-        // Configurable parameters
+        // Configurable parameters.
         val width = System.getenv("SA_WIDTH").let { if (it == null) 256 else it.toInt() }
         val height = System.getenv("SA_HEIGHT").let { if (it == null) 256 else it.toInt() }
         val iterations = System.getenv("SA_ITER").let { if (it == null) 10000000L else it.toLong() }
@@ -25,7 +26,7 @@ object SAImage {
         val output = System.getenv("SA_OUTPUT") ?: progress
         val rng = SecureRandom()
 
-        // Run the heavy computation
+        // Run the heavy computation.
         val problem = PixelProblem(width, height, rng)
 
         problem.callback = { n, e, retry ->
@@ -38,17 +39,15 @@ object SAImage {
             }
         }
 
-        val solver = Solver(problem, ExponentialDecayScheduler(temp, iterations), rng)
-
         val outfile = File(output ?: String.format("simulated-annealing-time%d-iters%d-starttemp%.1f.bmp",
                 System.currentTimeMillis(), iterations, temp))
 
-        solver.setListener { t, n, state, e ->
+        val solver = Solver(problem, ExponentialDecayScheduler(temp, iterations), rng, MinimumListener { t, n, state, e ->
             val image = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
             image.setRGB(0, 0, width, height, problem.data, 0, width)
             ImageIO.write(image, "bmp", outfile)
             println("MIN\t#$n\tE=$e")
-        }
+        })
 
         solver.solve()
     }
